98c3d1e73e2ba05076d1a734810f4d910577657b
mdsawye <mdsaywe@gmail.com>
2016-02-11 21:06:22 -0600

    2/11/16

---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var userSchema   = new Schema({
+    name: String,
+    login: String,
+    password: String
+
+});
+
+module.exports = mongoose.model('User', userSchema);
