3879f160db50e852eb475766bc48a8ff637914cb
jon <jon@idriveyourcar.com>
2016-03-03 20:28:10 +0100

    validation form in our controller

--- angular/app/Controllers/UserFormController.js
+++ angular/app/Controllers/UserFormController.js
@@ -2,11 +2,34 @@ angular
     .module('politics')
     .controller('UsrFrmCtrl', function ($scope, UserServices) {
         $scope.user = {};
+        $scope.loginErrorText = false;
+        $scope.passwordErrorText = false;
 
         $scope.SubmitForm = function () {
-            UserServices.adduser($scope.user).then(function(){
-                alert("user has been created")
-            })
+            if(checkForm() == true) {
+                insertUser();
+            }
+        };
 
+        var insertUser = function () {
+            UserServices.adduser($scope.user).then(function () {
+                alert("user has been created")
+            });
+        };
+
+        var checkForm = function () {
+            var result = true;
+
+            if ($scope.user.name == undefined) {
+                $scope.loginErrorText = true;
+                result = false;
+            }
+
+            if($scope.user.password == undefined) {
+                $scope.passwordErrorText = true;
+                result = false;
+            }
+
+            return result;
         }
     });
