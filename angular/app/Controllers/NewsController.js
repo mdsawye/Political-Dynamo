7e9bb4e9cfabcdd5bec6760e8e6332ad03f11de2
mdsawye <mdsaywe@gmail.com>
2016-08-12 10:32:55 -0500

    Fixed Party buttons

--- angular/app/Controllers/NewsController.js
+++ angular/app/Controllers/NewsController.js
@@ -1,13 +1,25 @@
 angular
     .module('politics')
-    .controller('NewsCtrl', function($scope, $location, NewsServices) {
+    .controller('NewsCtrl', function($scope, $location, $window, NewsServices) {
         console.log("This is the news controller")
         $scope.go = function(path) {
             $location.path('News' + path);
         }
 
-        $scope.Dem = function(assign) {
+        $scope.Dem = function() {
             $window.location.assign('https://www.democrats.org/');
+        }
+        $scope.Rep = function() {
+            $window.location.assign('https://www.gop.com/')
+        }
+        $scope.Lib = function() {
+            $window.location.assign('http://www.lp.org/')
+        }
+        $scope.Gre = function() {
+            $window.location.assign('http://www.gp.org/')
+        }
+        $scope.Con = function() {
+            $window.location.assign('http://www.constitutionparty.com/')
         }
 
 
